ff20014f2790889bc65d88dade5e8f1dbfce2473
tbc-beren <beren.themoonlightshade@gmail.com>
2019-06-05 19:33:46 +0200

    Updated Headers for license update

--- include/BCJson/BCJsonTypes.hpp
+++ include/BCJson/BCJsonTypes.hpp
@@ -1,3 +1,13 @@
+/**
+*
+* The Black Codex Library: Chapter: BCJson - A minimal JSON implementation
+*
+* https://github.com/tbc-beren/BCJson
+*
+* Mozilla Public License Version 2.0
+* https://github.com/tbc-beren/BCJson/blob/master/LICENSE
+*
+*/
 #pragma once
 
 namespace BlackCodex {
