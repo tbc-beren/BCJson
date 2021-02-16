586bb76f0e0f94e2b5b81d6983393aee6ae77222
tbc-beren <beren.themoonlightshade@gmail.com>
2021-02-16 19:30:24 +0100

    Add header and license notice to test files

--- test/testValue.cpp
+++ test/testValue.cpp
@@ -1,3 +1,17 @@
+/**
+*
+* The Black Codex Library: Chapter: BCJson - A minimal JSON implementation
+*
+* https://github.com/tbc-beren/BCJson
+*
+* Mozilla Public License Version 2.0
+* https://github.com/tbc-beren/BCJson/blob/master/LICENSE
+*
+* This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+* If a copy of the MPL was not distributed with this file, You can obtain one at
+* https://mozilla.org/MPL/2.0/.
+*
+*/
 #include <BCJson/BCJson.hpp>
 
 #include "testTools.h"
